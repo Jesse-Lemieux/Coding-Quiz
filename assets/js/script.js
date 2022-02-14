02efcd292ce8caffcd2b1ce3c8c7560c20bf93bf
JESSE <dtrsjesse121@gmail.com>
2022-02-14 14:36:30 -0500

    Reworked and refactored timer code and added more notes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,34 @@
+//Main elements and Global variables
 var bodyEl = document.querySelector('#body')
 var headerEl = document.querySelector('#header');
 var timerContainer = document.querySelector('#timerparent');
 var mainEl = document.querySelector('#main');
 
-//Timer functionality
-var timeLeft = 60;
-    var elem = document.getElementById('timer');
-    
-    var timerId = setInterval(countdown, 1000);
-    
-    function countdown() {
-      if (timeLeft == -1) {
-        clearTimeout(timerId);
-        doSomething();
-      } else {
-        elem.innerHTML = timeLeft + ' seconds remaining';
-        timeLeft--;
-      }
-    }
+//Timer element display
+var timeLeft = 75;
 
+var timerDisplay = document.createElement('h2');
+    timerDisplay.textContent = timeLeft + ' seconds remaining';
+    timerDisplay.className = 'timerdisplay';
+    timerContainer.appendChild(timerDisplay);
+//Timer functionality
+function quizTimer(){setInterval(() => {
+
+  if (timeLeft > 0){
+    timeLeft=timeLeft-1;
+    timerDisplay.innerHTML = timeLeft + " seconds remaining"
+  } 
+  else (clearInterval)
+}, 1000);
+}
+
+
+//Start button
 var quizStart = document.createElement('button');
     quizStart.textContent = 'START';
     quizStart.className = 'quizstart';
     mainEl.appendChild(quizStart);
-    quizStart.addEventListener('click');{
-        countdown();
-    }
+    quizStart.addEventListener('click', quizTimer);
+  
 
 
